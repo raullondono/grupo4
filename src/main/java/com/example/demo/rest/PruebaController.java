b673910bdee0eb364fac5479062d84f541278f42
Raul Londono Murillo <raul.londono@mercadolibre.com.co>
2023-05-12 10:01:47 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/rest/PruebaController.java
@@ -0,0 +1,15 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/prueba")
+public class PruebaController{
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World. probando CI/CD automaticos. Ahora debe publicar de manera automatica";
+    }
+}
